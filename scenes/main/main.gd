f6f4bac050998ed41c456eca835c596d1f346c3f
Craig Holbrook <craigholbrook33@gmail.com>
2024-01-15 21:41:18 -0500

    added upnp to forward port

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,12 +2,18 @@ extends Node
 
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
+var PORT = 808
 
 
-#port needs to be forwarded
+func _ready():
+	var upnp = UPNP.new()
+	upnp.discover()
+	upnp.add_port_mapping(808)
+
+
 #hosts external IP needs to be passed to create_client
 func _on_host_pressed():
-	peer.create_server(445)
+	peer.create_server(808)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
 	_add_player()
@@ -20,5 +26,5 @@ func _add_player(id = 1):
 
 
 func _on_join_pressed():
-	peer.create_client("localhost", 445)
+	peer.create_client("localhost", 808)
 	multiplayer.multiplayer_peer = peer
