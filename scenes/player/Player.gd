9ea30ec5a32f393b1fa48d002a0f9f0f46af55ab
Craig Holbrook <craigholbrook33@gmail.com>
2024-02-06 13:23:09 -0500

    multiplayer spawner, server authority on spawning and freeing, disconnect  signal implemented

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -4,11 +4,19 @@ class_name Player
 var id: int
 
 
-func _ready():
+func _enter_tree():
+	id = name.to_int()
 	set_multiplayer_authority(id)
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 400
 		move_and_slide()
+		if Input.is_action_just_pressed("ui_accept"):
+			del.rpc_id(1)
+
+
+@rpc("any_peer", "call_local")
+func del():
+	queue_free()
--- scenes/stage/stage.gd
+++ scenes/stage/stage.gd
@@ -1,15 +1,42 @@
 extends Node
 
-@onready var player_scene: PackedScene = load("res://scenes/player/player.tscn")
-
 
 func _ready():
+	spawn_old()
+	multiplayer.peer_disconnected.connect(del_player)
+
+	#await get_tree().create_timer(3.0).timeout
+	#if multiplayer.get_unique_id() == 1:
+	#del.rpc()
+
+
+func del_player(id: int):
+	if multiplayer.is_server():
+		if get_node(str(id)):
+			get_node(str(id)).queue_free()
+
+
+@rpc("any_peer", "call_local")
+func del():
+	get_tree().get_first_node_in_group("player").queue_free()
+
+
+func spawn_old():
+	if not multiplayer.is_server():
+		return
+
+	for id in GameManager.players:
+		var current_player: Player = load("res://scenes/player/player.tscn").instantiate()
+		current_player.name = str(id)
+		add_child(current_player, true)
+	spawn_pos.rpc()
+
+
+@rpc("any_peer", "call_local")
+func spawn_pos():
 	var index = 0
-	for i in GameManager.players:
-		var current_player: Player = player_scene.instantiate()
-		current_player.id = GameManager.players[i].id
-		add_child(current_player)
-		for spawn in get_tree().get_nodes_in_group("spawn_points"):
-			if spawn.name == str(index):
-				current_player.global_position = spawn.global_position
+	var spawns = get_tree().get_nodes_in_group("spawn_points")
+	for player: Player in get_tree().get_nodes_in_group("player"):
+		print(player.id)
+		player.global_position = spawns[index].global_position
 		index += 1
